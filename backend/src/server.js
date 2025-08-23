2952e309bddc4e4b6a3567ae37ebc7bd2803e30c
Shrivalkumar <shrival.24bcs10254@sst.scaler.com>
2025-08-23 19:06:03 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,37 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/connectDB.js';
+import {clerkMiddleware} from '@clerk/express';
+import { functions, inngest } from './config/inngest.js';
+import { serve } from "inngest/express";
 
 const app = express();
 
+app.use(express.json());
+app.use(clerkMiddleware()); // by using this req.auth will be availbale in the req object
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
 
-app.listen(ENV.PORT,()=>{
-    connectDB();
-    console.log(`Server is running on port ${ENV.PORT}`);
-})
+
+
+
+const startServer = async () =>{
+    try {
+        await connectDB();
+        if(ENV.NODE_ENV !== "production"){
+            app.listen(ENV.PORT,()=>{
+                console.log(`Server is running on port ${ENV.PORT}`);
+            })
+        }
+    } catch (error) {
+        console.error("Error starting the server")
+        process.exit(1);
+    }
+}
+
+startServer();
+
+export default app;
