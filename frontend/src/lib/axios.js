23bcabdb508fbd402e6eb3189a652a005769a206
Shrivalkumar <shrival.24bcs10254@sst.scaler.com>
2025-08-30 15:54:57 +0530

    AuthProvider and Sentry Tools setup

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const BASE_URL = import.meta.env.MODE === 'development' ? 'https://localhost:5001/api' : 'https://slack-backend-alpha.vercel.app/api'
+
+export const axiosInstance = axios.create({
+    baseURL : BASE_URL,
+    withCredentials: true,
+    
+})
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,23 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
-import {BrowserRouter} from "react-router";
+import {
+  createBrowserRouter,
+  createRoutesFromChildren,
+  matchRoutes,
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+} from "react-router";
+import { Toaster } from "react-hot-toast";
+import * as Sentry from "@sentry/react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./providers/AuthProvider.jsx";
+
+const queryClient = new QueryClient();
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,11 +25,32 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+
+Sentry.init({
+  dsn: "https://4e205ac3b2c15e93fd4955218e3f1775@o4509893345542144.ingest.us.sentry.io/4509932500942848",
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+          <Toaster position="top-right" />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
